4eb86e751e08e30d4c7b5bc805497a240d74e334
Tran Minh Hai <hai@bio-rithm.com>
2021-11-24 17:13:10 +0700

    add web icon

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+// 24 NOV 2021 TRAN MINH HAI test reproduce navbar 
+// Link target="_blank" means open in a new tab 
 import NextLink from 'next/link'
-import { Container, Box, Heading, Image, Link, useColorModeValue } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, Link, useColorModeValue, List, ListItem, Icon, SimpleGrid, Button } from "@chakra-ui/react"
 import Paragraph from "../components/paragraph"
 import Section from "../components/section"
 import { BioSection, BioYear } from '../components/bio'
-
+import { GridItem } from '../components/grid-item'
+import { IoLogoInstagram, IoLogoTwitter, IoLogoGithub, IoLogoDiscord } from 'react-icons/io5'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import thumbYouTube from './../public/images/links/youtube.png'
+import thumbInkdrop from './../public/images/works/inkdrop_eyecatch.png'
 
 const Page = () => {
   return (
@@ -106,6 +112,91 @@ const Page = () => {
           </Link>
           , Leica, Machine Learning
         </Paragraph>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as={"h3"} variant={"section-title"}>
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoGithub} />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/inkdrop_app" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoTwitter} />}
+              >
+                @inkdrop_app
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoTwitter} />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoInstagram} />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://discord.gg/QfsG5Kj" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoDiscord} />}
+              >
+                Discord
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.youtube.com/devaslife"
+            title="Dev as Life"
+            thumbnail={thumbYouTube}
+          >
+            My YouTube channel
+          </GridItem>
+          <GridItem
+            href="https://www.inkdrop.app/"
+            title="Inkdrop"
+            thumbnail={thumbInkdrop}
+          >
+            A Markdown note-taking app
+          </GridItem>
+        </SimpleGrid>
+        <Box align="center" my={4}>
+          <NextLink href="/posts">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Popular posts
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
     </Container>
   )
