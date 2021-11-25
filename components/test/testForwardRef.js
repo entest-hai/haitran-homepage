014fdcadac7f5c8c678f3facad126bc75c60e218
Tran Minh Hai <hai@bio-rithm.com>
2021-11-25 21:36:57 +0700

    check container.appendChild(document.createTextNode())

--- components/test/testForwardRef.js
+++ components/test/testForwardRef.js
@@ -1,5 +1,6 @@
 // 25 NOV 2021 TRAN MINH HAI 
 // test useRef and forwardRef 
+// appendChild(document.createTextNode('some text here'))
 
 import { Box } from '@chakra-ui/react';
 import { forwardRef, useRef, useEffect } from 'react';
@@ -18,14 +19,14 @@ const  TestChildForwardRef = forwardRef(({children}, ref) => (
 
 const TestParentForwardRef = () => {
   const ref = useRef()
-
   useEffect(() => {
     const {current: container} = ref
     console.log('child container ', container.style.height)
-    container.style.width = '100px'
+    container.style.width = '90%'
+    container.appendChild(document.createTextNode('Hi there how are you doing today?'));
 
   }, [])
-
+ 
   return (
     <TestChildForwardRef
       ref={ref}
