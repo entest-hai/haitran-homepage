958401db99e34e5fe78a1a2cc8d6f9db3d11d6ca
Tran Minh Hai <hai@bio-rithm.com>
2021-12-03 21:43:13 +0700

    for singapore

--- pages/index.js
+++ pages/index.js
@@ -1,205 +1,153 @@
-// 24 NOV 2021 TRAN MINH HAI test reproduce navbar 
-// Link target="_blank" means open in a new tab 
-import NextLink from 'next/link'
-import { Container, Box, Heading, Image, Link, useColorModeValue, List, ListItem, Icon, SimpleGrid, Button } from '@chakra-ui/react'
-import Paragraph from '../components/paragraph'
-import Section from '../components/section'
-import { BioSection, BioYear } from '../components/bio'
-import { GridItem } from '../components/grid-item'
-import { IoLogoInstagram, IoLogoTwitter, IoLogoGithub, IoLogoDiscord } from 'react-icons/io5'
-import { ChevronRightIcon } from '@chakra-ui/icons'
-import thumbYouTube from './../public/images/links/youtube.png'
-import thumbInkdrop from './../public/images/works/inkdrop_eyecatch.png'
+import {
+  Container,
+  Stack,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Icon,
+  IconButton,
+  createIcon,
+  IconProps,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
-const Page = () => {
+const Home =  () => {
   return (
-    <Container maxWidth={'container.md'}>
-      <Box 
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3} 
-        mb={6} 
-        textAlign="center">
-        Hello, I&apos;m a full-stack developer based in Vietnam!
-      </Box>
-      <Box display={{md: 'flex'}}>
-        <Box flexGrow={1} >
-          <Heading as="h2" variant="page-title">
-            Hai Tran
+    <Container maxW={'7xl'}>
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+        direction={{ base: 'column', md: 'row' }}>
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: '\'\'',
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'red.400',
+                zIndex: -1,
+              }}>
+              Biorithm Singapore,
+            </Text>
+            <br />
+            <Text as={'span'} color={'red.400'}>
+              Southest Asia!
+            </Text>
           </Heading>
-          <p>Web Developer / Signal Processing Engineer</p>
-        </Box>
-        <Box 
-          flexShrink={0} 
-          mt={{base:4, md:0}} 
-          ml={{md:6}} 
-          textAlign={'center'}
-        >
-          <Image
-            borderColor={'whiteAlpha.70'}
-            borderWidth={2}
-            borderStyle={'solid'}
-            maxW={'100px'}
-            display={'inline-block'}
-            borderRadius={'full'}
-            src={'/images/hai.jpg'}
-            alt={'Profile Image'}
-          >
-          </Image>
-        </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading
-          variant={'section-title'}
-        >
-          Work
-        </Heading>
-        <Paragraph>
-          Hai is a full-stack web developer and a signal processing engineer based in Hanoi, Vienam with a passion for building digital services/stuff that helpful for users. He belives that products can be realesed with a small team by using new and productive technologies. His tech stack is AWS Amplify, React, NextJS, JavaScript, Python and kee learning at best. Currently, he is creating a web {' '}
-          <NextLink href="/test">
-            <Link>entest</Link>
-          </NextLink> {' '}
-          for practicing IELTS and TOEFL tests where authors/gurus can also contribute contents. 
-        </Paragraph>
-      </Section>
-      <Section delay={0.2}>
-        <Heading 
-          as="h3" 
-          variant={'section-title'}>
-            Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1988</BioYear>
-          Born in Hanam, Vietnam.
-        </BioSection>
-        <BioSection>
-          <BioYear>
-            2016
-          </BioYear>
-          Completed the Doctor&apos;s Program in Information Engineering at Univ. of The Ryukyus, Japan
-        </BioSection>
-        <BioSection>
-          <BioYear>
-            2016
-          </BioYear>
-          Worked at VTTEK, Viettel, Vietnam 
-        </BioSection>
-        <BioSection>
-          <BioYear>
-            2018
-          </BioYear>
-          Worked at VinID, Vingroup, Vietnam 
-        </BioSection>
-        <BioSection>
-          <BioYear>
-            2019 to present
-          </BioYear>
-          Work at Biorithm, Singapore   
-        </BioSection>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as={'h3'} variant={'section-title'}>
-          I ♥
-        </Heading>
-        <Paragraph>
-          Art, Music,{' '}
-          <Link href="https://illust.odoruinu.net/" target="_blank">
-            Drawing
-          </Link>
-          , Playing Drums,{' '}
-          <Link href="https://500px.com/p/craftzdog" target="_blank">
-            Photography
-          </Link>
-          , Leica, Machine Learning
-        </Paragraph>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as={'h3'} variant={'section-title'}>
-          On the web
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost" 
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoGithub} />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/inkdrop_app" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoTwitter} />}
-              >
-                @inkdrop_app
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoTwitter} />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://instagram.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoInstagram} />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://discord.gg/QfsG5Kj" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<Icon as={IoLogoDiscord} />}
-              >
-                Discord
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            href="https://www.youtube.com/devaslife"
-            title="Dev as Life"
-            thumbnail={thumbYouTube}
-          >
-            My YouTube channel
-          </GridItem>
-          <GridItem
-            href="https://www.inkdrop.app/"
-            title="Inkdrop"
-            thumbnail={thumbInkdrop}
-          >
-            A Markdown note-taking app
-          </GridItem>
-        </SimpleGrid>
-        <Box align="center" my={4}>
-          <NextLink href="/posts">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              Popular posts
+          <Text color={'gray.500'}>
+            Users from AWS Asia Pacific (Singapore) ap-southest-1 will be routed here! 
+          </Text>
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={{ base: 'column', sm: 'row' }}>
+            <Button
+              rounded={'full'}
+              size={'lg'}
+              fontWeight={'normal'}
+              px={6}
+              colorScheme={'red'}
+              bg={'red.400'}
+              _hover={{ bg: 'red.500' }}>
+              Get started
             </Button>
-          </NextLink>
-        </Box>
-      </Section>
+            <Button
+              rounded={'full'}
+              size={'lg'}
+              fontWeight={'normal'}
+              px={6}
+              leftIcon={<PlayIcon h={4} w={4} color={'gray.300'} />}>
+              How It Works
+            </Button>
+          </Stack>
+        </Stack>
+        <Flex
+          flex={1}
+          justify={'center'}
+          align={'center'}
+          position={'relative'}
+          w={'full'}>
+          <Blob
+            w={'150%'}
+            h={'150%'}
+            position={'absolute'}
+            top={'-20%'}
+            left={0}
+            zIndex={-1}
+            color={useColorModeValue('red.50', 'red.400')}
+          />
+          <Box
+            position={'relative'}
+            height={'300px'}
+            rounded={'2xl'}
+            boxShadow={'2xl'}
+            width={'full'}
+            overflow={'hidden'}>
+            <IconButton
+              aria-label={'Play Button'}
+              variant={'ghost'}
+              _hover={{ bg: 'transparent' }}
+              icon={<PlayIcon w={12} h={12} />}
+              size={'lg'}
+              color={'white'}
+              position={'absolute'}
+              left={'50%'}
+              top={'50%'}
+              transform={'translateX(-50%) translateY(-50%)'}
+            />
+            <Image
+              alt={'Hero Image'}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={'100%'}
+              src={
+                'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+              }
+            />
+          </Box>
+        </Flex>
+      </Stack>
     </Container>
-  )
+  );
 }
 
-export default Page
+const PlayIcon = createIcon({
+  displayName: 'PlayIcon',
+  viewBox: '0 0 58 58',
+  d:
+    'M28.9999 0.562988C13.3196 0.562988 0.562378 13.3202 0.562378 29.0005C0.562378 44.6808 13.3196 57.438 28.9999 57.438C44.6801 57.438 57.4374 44.6808 57.4374 29.0005C57.4374 13.3202 44.6801 0.562988 28.9999 0.562988ZM39.2223 30.272L23.5749 39.7247C23.3506 39.8591 23.0946 39.9314 22.8332 39.9342C22.5717 39.9369 22.3142 39.8701 22.0871 39.7406C21.86 39.611 21.6715 39.4234 21.5408 39.1969C21.4102 38.9705 21.3421 3.7133 21.3436 38.4519V19.5491C21.3421 19.2877 21.4102 19.0305 21.5408 18.8041C21.6715 18.5776 21.86 18.3899 22.0871 18.2604C22.3142 18.1308 22.5717 18.064 22.8332 18.0668C23.0946 18.0696 23.3506 18.1419 23.5749 18.2763L39.2223 27.729C39.4404 27.8619 39.6207 28.0486 39.7458 28.2713C39.8709 28.494 39.9366 28.7451 39.9366 29.0005C39.9366 29.2559 39.8709 29.507 39.7458 29.7297C39.6207 29.9523 39.4404 30.1391 39.2223 30.272Z',
+});
+
+export const Blob = (props) => {
+  return (
+    <Icon
+      width={'100%'}
+      viewBox="0 0 578 440"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
+        fill="currentColor"
+      />
+    </Icon>
+  );
+};
+
+
+export default Home; 
---
+++ pages/index_backup.js
@@ -0,0 +1,205 @@
+// 24 NOV 2021 TRAN MINH HAI test reproduce navbar 
+// Link target="_blank" means open in a new tab 
+import NextLink from 'next/link'
+import { Container, Box, Heading, Image, Link, useColorModeValue, List, ListItem, Icon, SimpleGrid, Button } from '@chakra-ui/react'
+import Paragraph from '../components/paragraph'
+import Section from '../components/section'
+import { BioSection, BioYear } from '../components/bio'
+import { GridItem } from '../components/grid-item'
+import { IoLogoInstagram, IoLogoTwitter, IoLogoGithub, IoLogoDiscord } from 'react-icons/io5'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import thumbYouTube from './../public/images/links/youtube.png'
+import thumbInkdrop from './../public/images/works/inkdrop_eyecatch.png'
+
+const Page = () => {
+  return (
+    <Container maxWidth={'container.md'}>
+      <Box 
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3} 
+        mb={6} 
+        textAlign="center">
+        Hello, I&apos;m a full-stack developer based in Vietnam!
+      </Box>
+      <Box display={{md: 'flex'}}>
+        <Box flexGrow={1} >
+          <Heading as="h2" variant="page-title">
+            Hai Tran
+          </Heading>
+          <p>Web Developer / Signal Processing Engineer</p>
+        </Box>
+        <Box 
+          flexShrink={0} 
+          mt={{base:4, md:0}} 
+          ml={{md:6}} 
+          textAlign={'center'}
+        >
+          <Image
+            borderColor={'whiteAlpha.70'}
+            borderWidth={2}
+            borderStyle={'solid'}
+            maxW={'100px'}
+            display={'inline-block'}
+            borderRadius={'full'}
+            src={'/images/hai.jpg'}
+            alt={'Profile Image'}
+          >
+          </Image>
+        </Box>
+      </Box>
+      <Section delay={0.1}>
+        <Heading
+          variant={'section-title'}
+        >
+          Work
+        </Heading>
+        <Paragraph>
+          Hai is a full-stack web developer and a signal processing engineer based in Hanoi, Vienam with a passion for building digital services/stuff that helpful for users. He belives that products can be realesed with a small team by using new and productive technologies. His tech stack is AWS Amplify, React, NextJS, JavaScript, Python and kee learning at best. Currently, he is creating a web {' '}
+          <NextLink href="/test">
+            <Link>entest</Link>
+          </NextLink> {' '}
+          for practicing IELTS and TOEFL tests where authors/gurus can also contribute contents. 
+        </Paragraph>
+      </Section>
+      <Section delay={0.2}>
+        <Heading 
+          as="h3" 
+          variant={'section-title'}>
+            Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1988</BioYear>
+          Born in Hanam, Vietnam.
+        </BioSection>
+        <BioSection>
+          <BioYear>
+            2016
+          </BioYear>
+          Completed the Doctor&apos;s Program in Information Engineering at Univ. of The Ryukyus, Japan
+        </BioSection>
+        <BioSection>
+          <BioYear>
+            2016
+          </BioYear>
+          Worked at VTTEK, Viettel, Vietnam 
+        </BioSection>
+        <BioSection>
+          <BioYear>
+            2018
+          </BioYear>
+          Worked at VinID, Vingroup, Vietnam 
+        </BioSection>
+        <BioSection>
+          <BioYear>
+            2019 to present
+          </BioYear>
+          Work at Biorithm, Singapore   
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as={'h3'} variant={'section-title'}>
+          I ♥
+        </Heading>
+        <Paragraph>
+          Art, Music,{' '}
+          <Link href="https://illust.odoruinu.net/" target="_blank">
+            Drawing
+          </Link>
+          , Playing Drums,{' '}
+          <Link href="https://500px.com/p/craftzdog" target="_blank">
+            Photography
+          </Link>
+          , Leica, Machine Learning
+        </Paragraph>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as={'h3'} variant={'section-title'}>
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost" 
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoGithub} />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/inkdrop_app" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoTwitter} />}
+              >
+                @inkdrop_app
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoTwitter} />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoInstagram} />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://discord.gg/QfsG5Kj" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoDiscord} />}
+              >
+                Discord
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.youtube.com/devaslife"
+            title="Dev as Life"
+            thumbnail={thumbYouTube}
+          >
+            My YouTube channel
+          </GridItem>
+          <GridItem
+            href="https://www.inkdrop.app/"
+            title="Inkdrop"
+            thumbnail={thumbInkdrop}
+          >
+            A Markdown note-taking app
+          </GridItem>
+        </SimpleGrid>
+        <Box align="center" my={4}>
+          <NextLink href="/posts">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Popular posts
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+    </Container>
+  )
+}
+
+export default Page
