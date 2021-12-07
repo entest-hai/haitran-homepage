1204338e398a3ee5d24f517935f7f3d7e2801ef6
Tran Minh Hai <hai@bio-rithm.com>
2021-12-07 16:40:51 +0700

    add controler

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,6 @@
+import { useMemo } from 'react'; 
 import { HStack,
+  Container,
   Avatar,
   VStack, 
   Heading, 
@@ -14,7 +16,18 @@ import { HStack,
   Spacer, 
   useBreakpointValue,
   LinkBox,
-  LinkOverlay} from '@chakra-ui/react'; 
+  Table, 
+  Flex,
+  Thead,
+  Tbody, 
+  Tr, 
+  Td,
+  Th,
+  LinkOverlay,
+  SimpleGrid,
+  IconButton,
+  Tooltip} from '@chakra-ui/react';
+import { useTable, Column } from 'react-table';
 import { FaHome, 
   FaCompass, 
   FaMusic, 
@@ -28,6 +41,105 @@ import { BsBoxArrowLeft } from 'react-icons/bs';
 import NextLink from 'next/link'; 
 import { chakra } from '@chakra-ui/react';
 import NextImage from 'next/image';
+import { FaShoppingCart } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { FaStepBackward, FaPlay, FaStepForward, FaVolumeUp,  } from 'react-icons/fa';
+import { FiRepeat } from 'react-icons/fi';
+import { RiPlayListFill } from 'react-icons/ri';
+
+
+
+const topPicks = [{
+  image: '/images/ginger-me.jpg',
+  name: 'Ginger Me',
+  producer: 'Rexx Dee',
+  category: 'Afropop, Fusion',
+  time: '03:52',
+}, {
+  image: '/images/body-no-be-firewood.jpg',
+  name: 'Body no be firewood',
+  producer: 'Oju Kan',
+  category: 'Ambient',
+  time: '04:35',
+}, {
+  image: '/images/evident.jpg',
+  name: 'Evident',
+  producer: 'Bad Man Flex',
+  category: 'Amapiano',
+  time: '03:48',
+}, {
+  image: '/images/bridge.jpg',
+  name: 'Ginger Me',
+  producer: 'Blessed Up',
+  category: 'Afropop, Fusion',
+  time: '03:18',
+}, {
+  image: '/images/trail.jpg',
+  name: 'Hello Jim',
+  producer: 'Ruth The Child',
+  category: 'RnB',
+  time: '02:59',
+}, {
+  image: '/images/yellow-car.jpg',
+  name: 'Invincible',
+  producer: 'Mega Squad',
+  category: 'Amapiano',
+  time: '05:16',
+}, {
+  image: '/images/plant.jpg',
+  name: 'Ripple',
+  producer: 'The Materials',
+  category: 'RnB',
+  time: '03:52',
+}, {
+  image: '/images/ginger-me.jpg',
+  name: 'Ginger Me',
+  producer: 'Rexx Dee',
+  category: 'Afropop, Fusion',
+  time: '03:52',
+}, {
+  image: '/images/body-no-be-firewood.jpg',
+  name: 'Body no be firewood',
+  producer: 'Oju Kan',
+  category: 'Ambient',
+  time: '04:35',
+}, {
+  image: '/images/evident.jpg',
+  name: 'Evident',
+  producer: 'Bad Man Flex',
+  category: 'Amapiano',
+  time: '03:48',
+}, {
+  image: '/images/ginger-me.jpg',
+  name: 'Ginger Me',
+  producer: 'Rexx Dee',
+  category: 'Afropop, Fusion',
+  time: '03:52',
+}, {
+  image: '/images/body-no-be-firewood.jpg',
+  name: 'Body no be firewood',
+  producer: 'Oju Kan',
+  category: 'Ambient',
+  time: '04:35',
+}, {
+  image: '/images/evident.jpg',
+  name: 'Evident',
+  producer: 'Bad Man Flex',
+  category: 'Amapiano',
+  time: '03:48',
+}, {
+  image: '/images/bridge.jpg',
+  name: 'Ginger Me',
+  producer: 'Blessed Up',
+  category: 'Afropop, Fusion',
+  time: '03:18',
+}, {
+  image: '/images/trail.jpg',
+  name: 'Hello Jim',
+  producer: 'Ruth The Child',
+  category: 'RnB',
+  time: '02:59',
+},];
 
 const navItems = [
   {
@@ -168,6 +280,380 @@ const genres = [
 const MyImage = chakra(NextImage, {
   shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt', 'quality'].includes(prop),
 })
+
+const HeaderCell = ({ value }) => {
+  return (
+    <Heading
+      size='xs'
+      textTransform='none'
+      color='gray.light'
+    >
+      {value}
+    </Heading>
+  );
+}
+
+
+const TopPicks = ( props ) => {
+  const data = useMemo(() => topPicks, []);
+  const columns = useMemo(() => [{
+    accessor: 'image',
+    width: '100px',
+    Cell: ({value}) => (
+      <VStack alignItems='flex-start' justifyContent='center'>
+        <MyImage
+          src={value}
+          width='56px'
+          height='56px'
+          minW='56px'
+          minH='56px'
+          rounded='xl'
+          objectFit='cover'
+          quality='75'
+        >
+        </MyImage>
+      </VStack>
+    )
+  },{
+    Header: () => <HeaderCell value='Name'></HeaderCell>, accessor:'name'
+  }, {
+    Header: () => <HeaderCell value='Producer'></HeaderCell>, accessor:'producer'
+  }, {
+    Header: () => <HeaderCell value='category'></HeaderCell>, accessor:'category'
+  }, {
+    Header: () => <HeaderCell value='Time'></HeaderCell>, accessor: 'time', width: '40px'
+  }, {
+    Header: () => <HeaderCell value='Buy'></HeaderCell>, id: 'buy', width: '40px', 
+    Cell: () => (
+      <IconButton
+        icon={<FaShoppingCart></FaShoppingCart>}
+        aria-label='Buy Song'
+        rounded='full'
+        {...brandRing}
+      >
+      </IconButton>
+    )
+  }], [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow} = useTable({columns, data})
+
+
+  return (
+    <VStack align='flex-start' height='full' {...props}>
+      <Heading>Top Picks</Heading>
+      <Flex overflowY='auto' width='full'>
+        <Table {...getTableProps()} position='relative'>
+          <Thead>
+            {headerGroups.map((headerGroup) => (
+              <Tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <Th width={column.width} {...column.getHeaderProps()} borderBottom={0} position='sticky' zIndex='banner' top={0} bg='black'>
+                    {column.render('Header')}
+                  </Th>
+                ))}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row)
+              return (
+                <Tr
+                  {...row.getRowProps()}
+                  transition='ease-out'
+                  transitionProperty='background'
+                  transitionDuration='normal'
+                  _hover={{
+                    background: 'gray.dark',
+                    cursor:'pointer'
+                  }}
+                >
+                  {row.cells.map((cell) => (
+                    <Td {...cell.getCellProps()} py={2} borderBottom={0} paddingInlineEnd={0} minW={cell.column.width}>
+                      {cell.render('Cell')}
+                    </Td>
+                  ))}
+                </Tr>
+              )
+            })}
+          </Tbody>
+        </Table>
+      </Flex>
+    </VStack>
+  );
+}
+
+const topProducers = [{
+  name: 'BoyFlex',
+  image: '/images/dog-1.jpg',
+}, {
+  name: 'WhyFly',
+  image: '/images/dog-2.jpg',
+}, {
+  name: 'DJ Space',
+  image: '/images/dog-3.jpg',
+}, {
+  name: 'Wonda Ace',
+  image: '/images/dog-4.jpg',
+}, {
+  name: 'West Boy',
+  image: '/images/dog-5.jpg',
+}, {
+  name: 'Suga Wyle',
+  image: '/images/dog-6.jpg',
+}]
+
+
+const Producer = ( {name, image, badge} ) => {
+  const size = useBreakpointValue({ base: '48px', '2xl': '80px' }, '2xl')
+  return (
+    <VStack>
+      <Box position='relative'>
+        <MyImage
+          src={image}
+          width={size}
+          height={size}
+          rounded='lg'
+          objectFit='cover'
+        />
+        {
+          badge && (
+            <Box
+              bg='pink.200'
+              position='absolute'
+              top={-3}
+              right={-3}
+              rounded='full'
+              width={6}
+              height={6}
+              borderWidth={4}
+              borderColor='black'
+            />
+          )}
+      </Box>
+      <Text fontSize='sma' fontWeight='medium'>{name}</Text>
+    </VStack>
+  );
+}
+
+
+const TopProducers = ( props ) => {
+  return (
+    <VStack {...props} align='flex-start' spacing={{base: 6, '2xl': 8}} height='full' pb={6}>
+      <Heading size='md'>Top Producer</Heading>
+      <SimpleGrid columns={3} gap={{base: 4, '2xl': 8}}>
+        {topProducers.map((producer, index) => (
+          <Producer name={producer.name} image={producer.image} key={index} badge={index === 0}></Producer> 
+        ))}        
+      </SimpleGrid>
+      <VStack flex={1} spacing={4} alignItems='flex-start' width='full'>
+        <Heading
+          color='gray.light'
+          fontWeight='normal'
+          textTransform='uppercase'
+          letterSpacing={3}
+          fontSize='sm'
+        >
+          Your selection 
+        </Heading>
+        <VStack
+          width='full'
+          flex={1}
+          bg='gray.dark'
+          alignItems='center'
+          justifyContent='center'
+          rounded='lg'
+        >
+          <Heading
+            color='gray.light'
+            fontWeight='normal'
+            fontSize='xl'
+          >
+            Nothing selected 
+          </Heading>
+        </VStack>
+      </VStack>
+    </VStack>
+  );
+}
+
+const MotionBox = motion(Box); 
+
+const Disc = () => {
+  return (
+    <MotionBox
+      animate={{
+        rotate: 360
+      }}
+      transition={{
+        duration: 3, 
+        repeat: Infinity,
+        type: 'tween',
+        ease: 'linear'
+      }}
+      rounded='full'
+      overflow='hidden'
+      position='relative'
+      maxW={20}
+      maxH={20}
+      width='full'
+      height='full'
+    >
+      <MyImage
+        src='/images/gradient-mesh.png'
+        width="80px"
+        height='80px'
+        objectFit='cover'
+      >
+      </MyImage>
+      <Flex
+        position='absolute'
+        inset={0}
+        alignItems='center'
+        justifyContent='center'
+      >
+        <Box
+          width={6}
+          height={6}
+          bg='gray.dark'
+          rounded='full'
+        >
+        </Box>
+      </Flex>
+    </MotionBox>
+  );
+}
+
+const PlayerControl = ({ label, icon, color }) => {
+  return (
+    <Tooltip label={label} placement="top">
+      <IconButton
+        aria-label={label}
+        icon={icon}
+        rounded='full'
+        color={color}
+        {...brandRing}
+      >
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+
+const SongInfor = () => {
+  return (
+    <HStack spacing={8}>
+      <VStack spacing={2} alignItems='flex-start'>
+        <Heading size='sm' >
+          As board as daylight
+        </Heading>
+        <Text fontSize={14} color='gray.light'>
+          Moonlight Walker
+        </Text>
+      </VStack>
+      <PlayerControl
+        label='Add to favorites'
+        icon={<FaHeart></FaHeart>}
+        color='pink.200'
+      >
+      </PlayerControl>
+    </HStack>
+  );
+}
+
+const PlayerControls = ({ controls }) => {
+  return (
+    <HStack spacing={4}>
+      {controls.map(({label, icon}) => (
+        <PlayerControl key={label} label={label} icon={icon}>
+        </PlayerControl>
+      ))}
+    </HStack>
+  );
+}
+
+
+const PlayerProgress = () => {
+  return (
+    <HStack spacing={3} flex={1}>
+      <Text fontSize={12} color='gray.light'>
+        01:45
+      </Text>
+      <Box
+        flex={1}
+        width='full'
+        height={1}
+        bg='gray.light'
+        position='relative'
+        rounded='full'
+        overflow='hidden'
+      >
+        <Box
+          width='75%'
+          height={1}
+          left={0}
+          insetY={0}
+          bg='pink.200'
+          rounded='full'
+        >
+        </Box>
+      </Box>
+      <Text fontSize={12} color='gray.light'>
+        04:30
+      </Text>
+    </HStack>
+  );
+}
+
+const Player = () => {
+  return (
+    <Box
+      width='full'
+      bg='gray.dark'
+      height={{base: 24, '2xl': 32}}
+      flexShrink={0}
+    >
+      <Container
+        centerContent
+        height='full'
+        maxW='7xl'
+      >
+        <HStack
+          width='full'
+          height='full'
+          justifyContent='flex-start'
+          spacing={12}
+        >
+          <Disc></Disc>
+          <SongInfor></SongInfor>
+          <PlayerControls
+            controls={[
+              {label: 'Previous song', icon: <FaStepBackward></FaStepBackward>},
+              {label: 'Play song', icon: <FaPlay></FaPlay>},
+              {label: 'Next song', icon: <FaStepForward></FaStepForward>}
+            ]}
+          >
+          </PlayerControls>
+          <PlayerProgress></PlayerProgress>
+          <PlayerControls
+            controls={[
+              {label: 'Control volume', icon: <FaVolumeUp></FaVolumeUp>},
+              {label: 'Enable repeat', icon: <FiRepeat></FiRepeat>},
+              {label: 'Playlist', icon: <RiPlayListFill></RiPlayListFill>}
+            ]}
+          >
+          </PlayerControls>
+        </HStack>
+      </Container>
+    </Box>
+  );
+}
+
 
 const GenresCarousel = () => {
 
@@ -402,7 +888,8 @@ const Content = () => {
           overflow='hidden'
           flex={1}
         >
-          Hai Tran
+          <TopPicks flex={1}></TopPicks>
+          <TopProducers flexShrink={0}></TopProducers>
         </HStack>
       </VStack>
     </HStack>
@@ -418,6 +905,7 @@ const Dashboard = () => {
       spacing={0}
     >
       <Content></Content>
+      <Player></Player>
     </VStack>
   );
 }
